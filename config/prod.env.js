1dae68226a1ced56323e6c42b7cf43ba601e3f3c
EtienneDG <dussert.girard@gmail.com>
2017-11-20 15:35:37 +0100

    Build

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,3 +2,17 @@
 module.exports = {
   NODE_ENV: '"production"'
 }
+// dev: {
+//   env: require('./dev.env'),
+//   port: process.env.PORT || 8082,
+//   autoOpenBrowser: true,
+//   assetsSubDirectory: 'static',
+//   assetsPublicPath: '/',
+//   proxyTable: {},
+//   // CSS Sourcemaps off by default because relative paths are "buggy"
+//   // with this option, according to the CSS-Loader README
+//   // (https://github.com/webpack/css-loader#sourcemaps)
+//   // In our experience, they generally work as expected,
+//   // just be aware of this issue when enabling this option.
+//   cssSourceMap: false
+// }
